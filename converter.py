674f0b096a8b6fd2c08f5891a64f3c45c6b89691
danill <danillural@mail.ru>
2020-05-30 22:31:06 +0500

    commit

--- converter.py
+++ converter.py
@@ -9,6 +9,8 @@ class Converter:
         self.flags = '0' * (16 - len(int_flags) + 2) + str(int_flags)[2:]
         self.is_answer = self.flags[0]
         self.name, self.q_type, position = self.parse_question()
+        if self.is_answer:
+            self.parse_body(position)
 
     def parse_header(self):
         header = struct.unpack("!6H", self.data[0:12])
@@ -17,11 +19,13 @@ class Converter:
     def parse_question(self):
         name, end = self.parse_name2(12)
         q_type, q_class = struct.unpack("!HH", self.data[end: end + 4])
+        print("QUESTION", name, q_type, q_class, end + 4)
         return name, q_type, end + 4
 
     def parse_name2(self, start):
         name_list = []
         position = start
+        # print("NAME_START", start)
         end = start
         flag = False
         while True:
@@ -36,10 +40,45 @@ class Converter:
                 if length == 0:
                     if not flag:
                         end = position + 1
+                        flag = True
                     break
                 position += 1
                 name_list.append(self.data[position: position + length])
                 position += length
         name = ".".join([i.decode('ascii') for i in name_list])
-        print(name_list)
         return name, end
+
+    def parse_body(self, start):
+        print("ANSWERS")
+        answer_list, end1 = self.parse_rr(start, 3)
+        print("AUTHORITY")
+        authority_list, end2 = self.parse_rr(end1, 4)
+        print("ADDITIONAL")
+        additional_list, end3 = self.parse_rr(end2, 5)
+
+    def parse_rr(self, start, number):
+        offset = start
+        rr_list = []
+        for i in range(self.header[number]):
+            name, end = self.parse_name2(offset)
+            # print(name, end)
+            offset = end
+            r_type, r_class, r_ttl, rd_length = struct.unpack("!2HIH", self.data[offset: offset + 10])
+            print(
+                "\tname {0}, type {1}, class {2}, ttl {3}, rd_len {4}".format(name, r_type, r_class, r_ttl, rd_length))
+
+            offset += 10
+            if r_type == 1:
+                ip = struct.unpack("!4B", self.data[offset: offset + 4])
+                print("\t\tIP", ip)
+                offset += 4
+                rr_list.append((name, r_type, r_ttl, ip))
+            elif r_type == 2:
+                dns_server_name, dns_name_end = self.parse_name2(offset)
+                print("\t\tDNS_NAME", dns_server_name, dns_name_end)
+                offset = dns_name_end
+                rr_list.append((name, r_type, r_ttl, dns_server_name))
+            else:
+                offset += rd_length
+
+        return rr_list, offset
--- server.py
+++ server.py
@@ -21,18 +21,22 @@ class Server:
                 else:
                     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as dns:
                         dns.bind(("192.168.1.153", 55555))
-                        dns.sendto(data, ("8.8.8.8", 53))
+                        dns.sendto(data, ("ns1.e1.ru", 53))
                         out = dns.recvfrom(1024)[0]
+                        self.print_packet(out)
 
                     sock.sendto(out, addr)
                     c2 = Converter(out)
                     self.cache.put('d', 'NS', 'test', 10000)
+            print("-" * 30)
 
     def print_packet(self, data):
-        for i in range(len(data)):
-            print("{0:4}".format(str(hex(data[i]))), end=" ")
+        i = 1
+        for b in data:
+            print("{0:4}".format(str(hex(b))), end=" ")
             if i % 16 == 0:
                 print()
+            i += 1
         print()
 
 
